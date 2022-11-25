64717a4737ed276c12ea9bc246e77184cc1fd12c
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-11-25 02:39:26 +0000

    version with switch (inc)

--- .ccls-cache/@home@runner@HW4P3LFDJTM/main.cpp
+++ .ccls-cache/@home@runner@HW4P3LFDJTM/main.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+  int t;
+  int i1; i2; i3; i4; i5; i6; i7; i8; i9; i10;
+  cin >> t;
+
+  switch (t) {
+    case 1:
+      cin >> i1;
+    case 2:
+      cin >> i1;
+      cin >> i2;
+    case 3:
+      cin >> i1
+      cin >> i2
+      cin >> i3
+    case 4:
+      cin >> i1;
+      cin >> i2;
+      cin >> i3;
+      cin >> i4;
+    case 5:
+      cin >> i1;
+      cin >> i2;
+      cin >> i3;
+      cin >> i4;
+      cin >> i5;
+    case 6:
+      cin >> i1;
+      cin >> i2;
+      cin >> i3;
+      cin >> i4;
+      cin >> i5;
+      cin >> i6;
+    case 7:
+      cin >> i1;
+      cin >> i2;
+      cin >> i3;
+      cin >> i4;
+      cin >> i5;
+      cin >> i6;
+      cin >> i7;
+    case 8:
+      cin >> i1;
+      cin >> i2;
+      cin >> i3;
+      cin >> i4;
+      cin >> i5;
+      cin >> i6;
+      cin >> i7;
+      cin >> i8;
+  }
+  
+  
 }
